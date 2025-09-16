94e7518e6b79ee07a930b7952734ddf06b14c777
Rida Malik <internalprogression@gmail.com>
2025-09-16 07:18:50 +0500

    Persistent highscore storage in file, learnt file read/write in Python

--- scoreboard.py
+++ scoreboard.py
@@ -7,12 +7,15 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        # self.highscore = 0
         self.color("white")
         self.penup()
         self.hideturtle()
         self.goto(0, 250) # Top Center, which means we will have to make changes to snake & food movement i think.
+        with open("data.txt", "r") as file:
+            self.highscore = int(file.read())
         self.update_score()
+                
 
     def update_score(self):
         self.clear()
@@ -25,6 +28,9 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+        with open("data.txt", "w") as file:
+            file.write(str(self.highscore)) # save new highscore
+            # Instructor Alt: file.write(f"{self.highscore}")
         self.score = 0
         self.update_score()
 
